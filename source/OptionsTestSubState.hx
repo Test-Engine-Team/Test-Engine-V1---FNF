0beb3976aee780185bad12e75e7e8a096f854358
team-504 <75187806+team-504@users.noreply.github.com>
2022-11-21 14:49:13 -0600

    first commit

---
+++ source/OptionsTestSubState.hx
@@ -0,0 +1,11 @@
+package;
+
+class OptionsTestSubState extends MusicBeatSubstate
+{
+	var textMenuItems:Array<String> = ['Master Volume', 'Sound Volume'];
+
+	public function new()
+	{
+		super();
+	}
+}
