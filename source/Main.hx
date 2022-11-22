1156f1c40e462f1ea00ce2ecfc755e1c89dc5eb5
team-504 <75187806+team-504@users.noreply.github.com>
2022-11-21 19:50:31 -0600

    revert

--- source/Main.hx
+++ source/Main.hx
@@ -9,8 +9,10 @@ class Main extends Sprite
 	public function new()
 	{
 		super();
-		addChild(new FlxGame(0, 0, TitleState, 1, 1000, 1000, false, false));
+		addChild(new FlxGame(0, 0, TitleState));
 
+		#if !mobile
 		addChild(new FPS(10, 3, 0xFFFFFF));
+		#end
 	}
 }
