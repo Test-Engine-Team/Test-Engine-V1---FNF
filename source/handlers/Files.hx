4389f0ee2b9d08e0b1391fb061812e7093adec94
Mackery <davishayden32@gmail.com>
2022-12-16 14:52:37 -0600

    some good stuff

--- source/handlers/Files.hx
+++ source/handlers/Files.hx
@@ -1,6 +1,9 @@
 package handlers;
+
+import lime.tools.AssetType;
 import states.menus.TitleState;
 import flixel.graphics.frames.FlxAtlasFrames;
+import flixel.FlxG;
 
 class Files{
     static var file:String;
@@ -45,5 +48,34 @@ class Files{
 
     public static function sparrowAtlas(image:String) {
         return FlxAtlasFrames.fromSparrow('assets/images/$image.png', 'assets/images/$image.xml');
+    }
+
+    //public static function packerAtlas
+
+    /*
+    public static function fileExists(name:String, type:AssetType) {
+        if(OpenFlAssets.exists(getPath(name, type)))
+			return true;
+        else 
+		    return false;
+    }
+    dumb stuff idk how to do properly*/
+
+    public static function txt(path:String, fileName:String) {
+        return file = 'assets/$path/$fileName.txt';
+    }
+
+    public static function randomSound(min:Int, max:Int, fileName:String) {
+        return file = 'assets/sounds/$fileName' + FlxG.random.int(min, max) + '.ogg';
+    }
+
+    public static function songInst(songName:String) {
+        var songFolder = songName.toLowerCase();
+        return file = 'assets/songs/$songFolder/Inst.ogg';
+    }
+
+    public static function songVoices(songName:String) {
+        var songFolder = songName.toLowerCase();
+        return file = 'assets/songs/$songFolder/Voices.ogg';
     }
 }
