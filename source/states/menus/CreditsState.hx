436b844a46f7213487241350779ab1c527fdb94f
MemeHovy <124418090+MemeHovy@users.noreply.github.com>
2023-02-13 09:01:46 -0500

    Create CreditsState.hx

---
+++ source/states/menus/CreditsState.hx
@@ -0,0 +1,11 @@
+package;
+
+import flixel.FlxState;
+
+class CreditsState extends FlxState {
+    static var curSelected:Int = 0;
+
+    var credits:Array<Array<String>> = [
+        ['Name',		'icon',		'description',	 'url',	'FFEDD9']
+    ];
+}
