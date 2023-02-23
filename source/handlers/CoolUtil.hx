80990293e4f9cab4705bef37806d99209ba53217
Leather128 <81660872+Leather128@users.noreply.github.com>
2023-02-22 21:08:42 -0500

    at least get your linux stuff right bro

--- source/handlers/CoolUtil.hx
+++ source/handlers/CoolUtil.hx
@@ -6,31 +6,27 @@ import lime.utils.Assets;
 
 using StringTools;
 
-class CoolUtil
-{
+class CoolUtil {
 	public static var state:String = 'TemplateState';
 
-	public static function coolTextFile(path:String):Array<String>
-	{
+	public static function coolTextFile(path:String):Array<String> {
 		var daList:Array<String> = Assets.getText(path).trim().split('\n');
 
-		for (i in 0...daList.length)
-		{
+		for (i in 0...daList.length) {
 			daList[i] = daList[i].trim();
 		}
 
 		return daList;
 	}
 
-	public static function numberArray(max:Int, ?min = 0):Array<Int>
-	{
+	public static function numberArray(max:Int, ?min = 0):Array<Int> {
 		var dumbArray:Array<Int> = [];
-		for (i in min...max)
-		{
+		for (i in min...max) {
 			dumbArray.push(i);
 		}
 		return dumbArray;
 	}
+
 	public static function error(error:String, name:String) {
 		FlxG.stage.window.alert(error, name);
 	}
@@ -45,13 +41,12 @@ class CoolUtil
 		return (color.startsWith("#") || color.startsWith("0x")) ? FlxColor.fromString(color) : FlxColor.fromString("#" + color);
 	}
 
-	public static function openLink(Link:String = 'https://google.com') {
+	public static function openLink(link:String = 'https://google.com') {
 		#if linux
-		Sys.command('/usr/bin/xdg-open', [Link, "&"]);
+		Sys.command('/usr/bin/xdg-open', [link]);
 		#else
-		FlxG.openURL(Link);
+		FlxG.openURL(link);
 		#end
-
 	}
 
 	public static function switchToCustomState(To:String) {
@@ -66,8 +61,7 @@ class CoolUtil
 			Actually make and modify the scroll and lerp shit in it's own function
 			instead of solely relying on changing the lerp on the fly
 	 */
-	inline public static function camLerpShit(lerp:Float):Float
-	{
+	inline public static function camLerpShit(lerp:Float):Float {
 		return lerp * (FlxG.elapsed / (1 / 60));
-	}	
+	}
 }
